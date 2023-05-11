8df44ec51d0deeb95ae3e4de6001caf93f5eae2e
Elham Sameem <126508470+elhamsameem@users.noreply.github.com>
2023-05-11 19:37:31 -0400

    Created Block20 folder

---
+++ Unit 2/Block20/app.js
@@ -0,0 +1,15 @@
+const root = document.getElementById("root");
+
+const h1 = document.createElement("h1");
+h1.innerText = "MONA LISA";
+
+root.appendChild(h1);
+
+const img = document.createElement("img");
+img.src = "./asset/mona_lisa.jpg";
+root.appendChild(img);
+
+
+const p = document.createElement("p");
+p.innerText = "The Mona Lisa is a famous painting";
+root.appendChild(p);
