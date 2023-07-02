6313f76039e3efae24fb884df42b21220532f6c9
Elhamudin Sameem <elhamsmacbookpro@elhamudins-mbp.myfiosgateway.com>
2023-07-02 12:22:07 -0400

    Implemented array.map() method with React

--- Unit 3/Block24 Intro to React and JSX/Workshop Puppy Pals/puppy pals workshop/src/App.jsx
+++ Unit 3/Block24 Intro to React and JSX/Workshop Puppy Pals/puppy pals workshop/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
+import { puppyList } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+  console.log("puppies list from useState: ", puppies);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+          {/* puppy list was howing error to keep the list organized so I added "key"
+              Resource: https://react.dev/learn/rendering-lists#keeping-list-items-in-order-with-key */}
+        })
+      }
+    </div>
   )
 }
 
