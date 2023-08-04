0caa0e6c48baaf09192598be5d377937ce94427c
Elhamudin Sameem <elhamsmacbookpro@elhamudins-mbp.myfiosgateway.com>
2023-08-03 22:47:06 -0400

    Added feat: AllPlayers component has been finished

--- Unit 3/Block29 Puppy Bowl React Project/Puppy-Bowl-React/src/components/AllPlayers.jsx
+++ Unit 3/Block29 Puppy Bowl React Project/Puppy-Bowl-React/src/components/AllPlayers.jsx
@@ -1,34 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { fetchAllPlayers } from "../API";
+import "./AllPlayers.css";
 
 function AllPlayers() {
   const [players, setPlayers] = useState(null);
+
+  // Fethcing all players using a function from API folder
   useEffect(() => {
     async function fetchPlayers() {
       const allPlayers = await fetchAllPlayers();
-      //   console.log(allPlayers);
       setPlayers(allPlayers);
     }
     fetchPlayers();
   }, []);
 
-  function render(player) {
-    console.log(player.name);
-    const name = player.name;
-    return name;
-  }
-
   return (
-    <>
-      <h1>All Players compo</h1>
-      {players && (
-        <div className="all-players">
-          {players.map((player) => {
-            return <p>{render(player)}</p>;
-          })}
-        </div>
+    <div className="all-players">
+      {players ? (
+        players.map((player) => {
+          return (
+            <div className="player-card" key={player.id}>
+              <h2 className="player-name">{player.name}</h2>
+              <div className="player-img-container">
+                <img src={player.imageUrl} alt="" />
+              </div>
+              <div className="buttons-div">
+                <button>See Details</button>
+                <button className="remove-button">Remove</button>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <p>players not found</p>
       )}
-    </>
+    </div>
   );
 }
 
